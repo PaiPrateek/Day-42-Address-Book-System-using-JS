c8fa9cc8eac00396fad11e1d81a94cdc7fb1fb7d
Prateek Pai <paiprateek123@gmail.com>
2022-07-31 16:57:13 +0530

    Create Addressbook Contact

--- AddressBook.js
+++ AddressBook.js
@@ -1,1 +1,29 @@
 console.log("Welcome to AddressBook System Using Javascript");
+//UC1 Ability to create a Address Book Contact with first and last names,address, city, state, zip, phone number and email...RG----
+class Contact{
+    firstName;
+    lastName;
+    address;
+    city;
+    state;
+    zip;
+    phoneNumber;
+    eMail;
+
+    constructor(...params){
+        this.firstName = params[0];
+        this.lastName = params[1];
+        this.address = params[2];
+        this.city = params[3];
+        this.state = params[4];
+        this.zip = params[5];
+        this.phoneNumber = params[6];
+        this.eMail = params[7];
+    }
+
+        toString(){
+            return ("First Name: "+this.firstName+" \nLast Name: "+this.lastName+"\nAddress: "+this.address+"\nCity : "+this.city+"\nState: "+this.state+"\nZip Code : "+this.zip+"\nPhone: "+this.phoneNumber+ "\nE-Mail: "+this.eMail);
+        }
+}
+let contactObj = new Contact("Prateek","Pai","Bangalore","Bangalore","Karnataka",560027,9945007207,"prateek@gmail.com");
+console.log(contactObj.toString());
